16506c2ca1ce0b4ba9327b5178cb7d38a38c1e4e
Sean Lightfoot <seanalexander360@gmail.com>
2021-09-28 13:39:45 -0500

    podcast page

--- frontend/castable/src/components/Podcast.js
+++ frontend/castable/src/components/Podcast.js
@@ -18,65 +18,69 @@ export default function Podcast(props) {
     <section>
       <div className="container">
         <div className="row bg-light rounded">
-          {loading && (
-            <div className="spinner-border" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </div>
-          )}
-          {currentPodcast.episodes && (
-            <div className="row p-2">
-              <img
-                src={currentPodcast.thumbnail}
-                alt={currentPodcast.title}
-                className="col img-fluid"
-              />
-              <div className="col">
-                <h1 className="display-5 text-dark">{currentPodcast.title}</h1>
-                <hr className="border-dark"></hr>
-                <p className="h3 text-body">{currentPodcast.description}</p>
-                <p className="h5">
-                  Number of episodes: {currentPodcast.total_episodes}
-                </p>
-                <p className="h5 text-muted">
-                  Publisher: {currentPodcast.publisher}
-                </p>
+          <div className="col">
+            {loading && (
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
               </div>
-              <div className="accordion" id="accordionExample">
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="headingOne">
-                    <button
-                      className="accordion-button"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseOne"
-                      aria-expanded="true"
-                      aria-controls="collapseOne"
+            )}
+            {currentPodcast.episodes && (
+              <div className="row p-3">
+                <img
+                  src={currentPodcast.thumbnail}
+                  alt={currentPodcast.title}
+                  className="col img-fluid"
+                />
+                <div className="col">
+                  <h1 className="display-5 text-dark">
+                    {currentPodcast.title}
+                  </h1>
+                  <hr className="border-dark"></hr>
+                  <p className="h3 text-body">{currentPodcast.description}</p>
+                  <p className="h5">
+                    Number of episodes: {currentPodcast.total_episodes}
+                  </p>
+                  <p className="h5 text-muted">
+                    Publisher: {currentPodcast.publisher}
+                  </p>
+                </div>
+                <div className="accordion" id="accordionExample">
+                  <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                      <button
+                        className="accordion-button"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne"
+                        aria-expanded="true"
+                        aria-controls="collapseOne"
+                      >
+                        Episodes
+                      </button>
+                    </h2>
+                    <div
+                      id="collapseOne"
+                      className="accordion-collapse collapse show p-2"
+                      aria-labelledby="headingOne"
+                      data-bs-parent="#accordionExample"
                     >
-                      Episodes
-                    </button>
-                  </h2>
-                  <div
-                    id="collapseOne"
-                    className="accordion-collapse collapse show p-2"
-                    aria-labelledby="headingOne"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div className="accordion-body row p-2">
-                      {currentPodcast.episodes.map((episode) => (
-                        <Episode
-                          thumbnail={episode.thumbnail}
-                          title={episode.title}
-                          key={episode.id}
-                          function={setCurrentSource}
-                          audio={episode.audio}
-                        />
-                      ))}
+                      <div className="accordion-body row p-2">
+                        {currentPodcast.episodes.map((episode) => (
+                          <Episode
+                            thumbnail={episode.thumbnail}
+                            title={episode.title}
+                            key={episode.id}
+                            function={setCurrentSource}
+                            audio={episode.audio}
+                          />
+                        ))}
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </section>
